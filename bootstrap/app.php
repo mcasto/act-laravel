0b9e1466f27c3a26ee67e462450560b7091b4e4a
Mike Casto <castoware@gmail.com>
2025-11-26 21:20:01 -0500

    Fixed routing problem (improper Request import in bootstrap/app)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
